7c3187bbcef88c9deece180aac4f25752a78b640
amanchouhan192 <amanchouhan192@gmail.com>
2021-10-25 18:38:15 +0530

    global  variable code in file globalvar.rb

---
+++ ruby/classes_and_objects/globalvar.rb
@@ -0,0 +1,21 @@
+$global_variable = 10
+class Class1
+   def print_global
+      puts "Global variable in Class1 is #$global_variable"
+   end
+end
+class Class2
+   def print_global
+      puts "Global variable in Class2 is #$global_variable"
+   end
+end
+
+class1obj = Class1.new
+class1obj.print_global
+class2obj = Class2.new
+class2obj.print_global
+
+=begin
+this is the multiple line comments
+starts with begin and ends with =end
+=end
