ca2f29052700f23eae5cd6ebfbec7a012aead86a
amanchouhan192 <amanchouhan192@gmail.com>
2021-10-27 18:44:23 +0530

    done with hash topic from tutorialpoints did hash calling by key ,hash key value and default hash value call.

---
+++ ruby/hashes/hash_call_key.rb
@@ -0,0 +1,10 @@
+months  = Hash.new("month")
+months = {"1"=>"January","2"=>"February"}
+
+keys = months.keys
+puts "#{keys}"
+
+#output:
+#
+#["1", "2"]
+
---
+++ ruby/hashes/hashes_default.rb
@@ -0,0 +1,9 @@
+months = Hash.new("month")
+
+puts "#{months[0]}"
+puts "#{months[72]}"
+
+#output :
+#
+#month
+#month
