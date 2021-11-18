e43ed03f7277f4c3a8225ba99677ae6f8a795064
amanchouhan192 <amanchouhan192@gmail.com>
2021-11-18 17:28:48 +0530

    done with create a new subject in form field,user can enter the subject into input field and the table will update when press create subject button pressed.

--- rails/simple_cms/app/controllers/subjects_controller.rb
+++ rails/simple_cms/app/controllers/subjects_controller.rb
@@ -9,9 +9,23 @@ class SubjectsController < ApplicationController
   end
 
   def new
+    @subject = Subject.new
+    #@subject = Subjects.new(:visible => false)
   end
 
   def create
+    #Instantiate a new object using form parameter
+    @subject = Subject.new(subject_params) # mass assignment 
+    #save the object
+    if @subject.save
+      #if save succeeds,redirect to the next index action
+      redirect_to(subjects_path)
+    else
+      #if save fails ,redisplay the form so user can fix problem.
+      render('new')
+    end
+
+
   end
 
   def edit
@@ -25,5 +39,11 @@ class SubjectsController < ApplicationController
 
   def destroy
   end
+
+  private 
+
+    def subject_params
+      params.require(:subject).permit(:name)
+    end
   
 end
