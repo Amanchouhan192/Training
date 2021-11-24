967e20ec5241443ada9a710b5a9dc7e631efaa3f
amanchouhan192 <amanchouhan192@gmail.com>
2021-11-24 16:10:54 +0530

    added validation for phone and email and added radio button for gender.

--- rails/Blogging/app/models/user.rb
+++ rails/Blogging/app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
    # has_secure_password
     validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i,message: "mist be a valid email address", on: :create }
-
+    validates_format_of :phone, :with =>  /\d[0-9]\)*\z/ , :message => "Only positive number without spaces are allowed"
 end
