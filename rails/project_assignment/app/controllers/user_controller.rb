82b244f2264e61eef5dfd4ccddd36c04730a0a9a
amanchouhan192 <amanchouhan192@gmail.com>
2021-11-27 18:59:26 +0530

    created user login and signup ,created sessions for user,integrated database.

---
+++ rails/project_assignment/app/controllers/user_controller.rb
@@ -0,0 +1,61 @@
+class UserController < ApplicationController
+
+
+    def new
+      @user = User.new
+      if session[:user_id] == nil
+        redirect_to '/user/login'         #for checking user is not logged in id is nil
+      else
+        # redirect_to '/user/logout'        #for check user is logged in already
+      end
+    end
+    
+    def create  #for signup things
+      #Instantiate a new object using form paramete
+         @user = User.new(user_params) # mass assignment 
+        #save the object
+      if @user.save 
+          #if save succeeds,redirect to the next index action
+        redirect_to '/user/new'   #this is redirecting to the new.html.erb page
+      else
+          #if save fails ,redisplay the form so user can fix problem.
+        render('new') 
+      end
+    
+    end
+
+    def login_create
+        @user = User.find_by(email: params[:user][:email]) 
+        if @user.present?
+            if @user.password == params[:user][:password]
+                session[:user_id] = @user.id  
+                redirect_to '/user/new'
+            end
+        end
+        
+    end
+
+    def signup
+        if session[:user_id] != nil
+            redirect_to '/user/new'
+        end
+    end
+
+    def login
+        if session[:user_id] != nil
+            redirect_to '/user/new'
+        end
+    end
+
+    def logout     
+        session[:user_id] = nil         
+        redirect_to '/user/login' 
+    end  
+    
+    private 
+    
+    def user_params
+        params.require(:user).permit(:name,:email,:password)
+    end
+      
+end
--- rails/project_assignment/config/routes.rb
+++ rails/project_assignment/config/routes.rb
@@ -1,12 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root to: 'home#index'
+  root to: 'user#index'
 
-  get 'home/index', to: 'home#index'
-  get 'home/login', to: 'home#login'
+  get 'user/index', to: 'user#index'
+  get '/user/login', to: 'user#login'
 
-  get 'home/signup', to: 'home#signup'
- post 'home/create', to: 'home#create'
-  get 'home/new',to: 'home#new'
+  post '/user/logout',to: 'user#logout'
+
+  
+
+  #get '/login',to: 'user#login'
+  post '/user/login_create', to: 'user#login_create'
+
+  get 'user/signup', to: 'user#signup'
+ post '/user/create', to: 'user#create'
+  get 'user/new',to: 'user#new'
+ post '/user/new',to: 'user#new'
 end
