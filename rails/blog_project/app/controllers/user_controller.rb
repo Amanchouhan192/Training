6ca4f6d72826885d165e0afbc0fe05b5f39950af
amanchouhan192 <amanchouhan192@gmail.com>
2021-11-29 12:29:28 +0530

    create actions index ,create,user_params,blog_params

--- rails/blog_project/app/controllers/user_controller.rb
+++ rails/blog_project/app/controllers/user_controller.rb
@@ -1,2 +1,22 @@
 class UserController < ApplicationController
+
+        def index
+          @users = User.all
+        end
+        
+        def create
+        end
+        
+        private 
+        
+        def user_params
+            params.require(:user).permit(:name,:email,:password,:gender,:age,:phone)
+        end
+          
+        def blog_params
+          params.permit(:title,:body)
+        end
+          
+    
+    
 end
