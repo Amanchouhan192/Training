4045e3be0988cc0b001b11722931b84f43dbee9d
amanchouhan192 <amanchouhan192@gmail.com>
2021-11-29 14:23:34 +0530

    added login post request route.

--- rails/blog_project/config/routes.rb
+++ rails/blog_project/config/routes.rb
@@ -2,11 +2,14 @@ Rails.application.routes.draw do
   root to: 'user#index'
 
   get 'user/index', to: 'user#index' # this will show  user index file
-  get 'user/login',to: 'user#login' # show login page 
+  get '/user/login',to: 'user#login' # show login page 
 
   get 'user/new',to: 'user#new'     #show new page after login i.e sfter  the user logged in 
+  get '/user/new',to: 'user#new'
 
   get 'user/signup',to: 'user#signup' #show the signup form for enter user datails
  post '/user/create',to: 'user#create' #signup form post rquest for enter detail into database
 
+ #get '/user/login_create', to: 'user#login_create' #create user login post request
+ post '/user/login_create', to: 'user#login_create' #create user login post request
 end
