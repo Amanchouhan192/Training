72bf66ee3035aec19f97b9d4ba7d6f258dea3fc2
amanchouhan192 <amanchouhan192@gmail.com>
2021-11-29 12:07:56 +0530

    create login page with no post request and index file modified with new action and new.html.erb view si created.

--- rails/blog_project/config/routes.rb
+++ rails/blog_project/config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   root to: 'user#index'
 
   get 'user/index', to: 'user#index' # this will show  user index file
+  get 'user/login',to: 'user#login' # show login page 
+
+  get 'user/new',to: 'user#new'     #show new page after login i.e sfter  the user logged in 
 end
