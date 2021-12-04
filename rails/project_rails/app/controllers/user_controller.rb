3de027f3707e253e9d0d7ddf0bfc9979a61cd37e
amanchouhan192 <amanchouhan192@gmail.com>
2021-12-04 18:12:21 +0530

    create signup create action in user controller.

--- rails/project_rails/app/controllers/user_controller.rb
+++ rails/project_rails/app/controllers/user_controller.rb
@@ -4,6 +4,19 @@ class UserController < ApplicationController
   end
   
   def create
-    
+    @user = User.new(user_params)
+    if @user.save
+        redirect_to ''
+    else
+        render('new')
+    end
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name,:email,:password,:gender,:age,:phone)
+  end
+
+
 end
