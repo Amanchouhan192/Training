ac86703f3e08359bb65df54ad18785208ec6fdac
amanchouhan192 <amanchouhan192@gmail.com>
2021-12-05 00:08:45 +0530

    edits in validaion of  user class.

--- rails/project_rails/app/models/user.rb
+++ rails/project_rails/app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
   validates :email, uniqueness: true
   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i,message: "must be a valid email address", on: :create }
   validates_format_of :phone, :with =>  /\d[0-9]\)*\z/ , :message => "Only positive number without spaces are allowed"
-  validates :phone,:presence => true,:numericality => true, :length => { :minimum => 10, :maximum => 15 }
+  validates :phone,:presence => true,:numericality => true, :length => { :minimum => 10, :maximum => 10 }
 end
