8c963e3a5fdcaf28fdc3ee77ed2bb98ad504e366
amanchouhan192 <amanchouhan192@gmail.com>
2021-12-05 01:20:34 +0530

    added helper methods.

--- rails/project_rails/app/controllers/application_controller.rb
+++ rails/project_rails/app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
+    helper_method :current_user
+    helper_method :logged_in?
+
+    def current_user   
+        User.find_by(id: session[:user_id])
+    end
+    
+    def logged_in?
+       !current_user.nil?
+    end
+    
     add_flash_types :danger, :info, :warning, :success #flash alert
+
 end
