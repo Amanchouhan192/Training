adbd317bc914ee4827ca93b74936c74637a04138
amanchouhan192 <amanchouhan192@gmail.com>
2021-12-04 23:20:19 +0530

    added flash message on new.html.erb file.

--- rails/project_rails/app/models/user.rb
+++ rails/project_rails/app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  validates :name, :email, presence: true 
+  validates :email, uniqueness: true
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i,message: "must be a valid email address", on: :create }
+  validates_format_of :phone, :with =>  /\d[0-9]\)*\z/ , :message => "Only positive number without spaces are allowed"
+  validates :phone,:presence => true,:numericality => true, :length => { :minimum => 10, :maximum => 15 }
 end
