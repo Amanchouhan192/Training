24021873b71c0fa06bfeed7b1e9f655a36d377ba
amanchouhan192 <amanchouhan192@gmail.com>
2021-12-04 23:16:00 +0530

    added flash alert for invalid entries.

--- rails/project_rails/app/controllers/user_controller.rb
+++ rails/project_rails/app/controllers/user_controller.rb
@@ -6,9 +6,9 @@ class UserController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-        redirect_to ''
+        redirect_to '/user/new',success: "User Ragistered Successfully!"
     else
-        render('new')
+        redirect_to '/user/new' ,danger: "Invalid Email or Phone Number you entered!"
     end
   end
 
