bc3924665c9af727953c3db96f19cc8100f95457
amanchouhan192 <amanchouhan192@gmail.com>
2021-12-05 16:22:13 +0530

    added confirmation passwprd param in user params.

--- rails/project_rails/app/models/user.rb
+++ rails/project_rails/app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i,message: "must be a valid email address", on: :create }
   validates_format_of :phone, :with =>  /\d[0-9]\)*\z/ , :message => "Only positive number without spaces are allowed"
   validates :phone,:presence => true,:numericality => true, :length => { :minimum => 10, :maximum => 10 }
+  validates :password, :confirmation => true, :length => { in: 6..10}
+  validates :password_confirmation, presence: true
 end
