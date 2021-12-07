983e5563317fcccf247462960d368929ba7d0333
amanchouhan192 <amanchouhan192@gmail.com>
2021-12-07 12:44:46 +0530

    added delete and destroy action and make code indent.

--- rails/project_rails/app/controllers/post_controller.rb
+++ rails/project_rails/app/controllers/post_controller.rb
@@ -1,57 +1,57 @@
 class PostController < ApplicationController
 
-  def index
-    @posts = Post.all
-  end
+	def index
+		@posts = Post.all
+	end
 
-  def show
-      @post = Post.find(params[:id])
-  end
-  
-  def new
-      @post = Post.new
-  end
-  
-  def create
-    #Instantiate a new object using form paramete
-      @post = Post.new(blog_params) # mass assignment 
-      #save the object
-      if @post.save 
-        #if save succeeds,redirect to the next index action
-        redirect_to '/post/index'
-      else
-        #if save fails ,redisplay the form so user can fix problem.
-        render('/post/index')
-      end
-  end
-  
-  def edit
-      @post = Post.find(params[:id])
-  end
-  
+	def show
+		@post = Post.find(params[:id])
+	end
+
+	def new
+		@post = Post.new
+	end
+
+	def create
+		#Instantiate a new object using form paramete
+		@post = Post.new(blog_params) # mass assignment 
+		#save the object
+		if @post.save 
+			#if save succeeds,redirect to the next index action
+			redirect_to '/post/index'
+		else
+			#if save fails ,redisplay the form so user can fix problem.
+			render('/post/index')
+		end
+	end
+
+	def edit
+		@post = Post.find(params[:id])
+	end
+		
   def update
-      @post = Post.find(params[:id])
-      if @post.update(blog_params)
-        redirect_to '/post/index'
-      else
-        render('edit')
-      end
-  end
-  
-  def delete
-      @post = Post.find(params[:id])
-  end 
-  
-  def destroy
-      @post = Post.find(params[:id])
-      @post.destroy
-      redirect_to '/post/index'
-      render 'delete'
-  end
-  
+		@post = Post.find(params[:id])
+		if @post.update(blog_params)
+			redirect_to '/post/index'
+		else
+			render('edit')
+		end
+	end
+
+	def delete
+		@post = Post.find(params[:id])
+	end 
+
+	def destroy
+		@post = Post.find(params[:id])
+		@post.destroy
+		redirect_to '/post/index'
+	end
+
   private 
-  
+		
   def blog_params
-      params.require(:post).permit(:title,:body)
-  end
+		params.require(:post).permit(:title,:body)
+	end
+
 end
