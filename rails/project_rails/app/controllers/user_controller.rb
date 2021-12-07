bc436f659436e091e50a60b1eaf6dfa0ee28753d
amanchouhan192 <amanchouhan192@gmail.com>
2021-12-07 12:47:27 +0530

    added indent code in user controller.

--- rails/project_rails/app/controllers/user_controller.rb
+++ rails/project_rails/app/controllers/user_controller.rb
@@ -1,47 +1,45 @@
 class UserController < ApplicationController
-  def new
-    @user = User.new
-  end
-  
-  def create
-    @user = User.new(user_params)
-    if @user.save
-        redirect_to '/user/new',success: "User Ragistered Successfully!"
-    else
-       #redirect_to '/user/new' ,danger: "Invalid Email or Phone Number you entered! or Password mismatch!"
-       render 'new'
-      end
-  end
+	def new
+		@user = User.new
+	end
+		def create
+		@user = User.new(user_params)
+		if @user.save
+			redirect_to '/user/new',success: "User Ragistered Successfully!"
+		else
+			#redirect_to '/user/new' ,danger: "Invalid Email or Phone Number you entered! or Password mismatch!"
+			render 'new'
+		end
+	end
 
-  def login_check
-    @user = User.find_by(email: params[:user][:email]) 
-    if @user.present?
-        if @user.password == params[:user][:password]
-            session[:user_id] = @user.id  
-            redirect_to '/post/index'
-        else
-            redirect_to '/user/login' ,danger: "Invalid Email or Password!"
-        end
-   else
-       redirect_to '/user/login' ,danger: "Invalid Email or Password!"
-   end
-  end
+	def login_check
+		@user = User.find_by(email: params[:user][:email]) 
+		if @user.present?
+			if @user.password == params[:user][:password]
+				session[:user_id] = @user.id  
+				redirect_to '/post/index'
+			else
+				redirect_to '/user/login' ,danger: "Invalid Email or Password!"
+			end
+		else
+			redirect_to '/user/login' ,danger: "Invalid Email or Password!"
+		end
+	end
 
-  def login
-    if session[:user_id] != nil
-        redirect_to '/post/index'
-    end
-  end
+	def login
+		if session[:user_id] != nil
+			redirect_to '/post/index'
+		end
+	end
 
-  def logout     
-    session[:user_id] = nil         
-    redirect_to '/user/login' 
-  end  
+	def logout     
+		session[:user_id] = nil         
+		redirect_to '/user/login' 
+	end  
 
-  private
+	private
 
-  def user_params
-    params.require(:user).permit(:name,:email,:password, :password_confirmation,:gender,:age,:phone)
-  end
-  
+	def user_params
+		params.require(:user).permit(:name,:email,:password, :password_confirmation,:gender,:age,:phone)
+	end
 end
