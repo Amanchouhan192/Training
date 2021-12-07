b7488b8fdf046f3a944a7716f6ee28a390206b12
amanchouhan192 <amanchouhan192@gmail.com>
2021-12-07 17:05:23 +0530

    created comment controller and helper files.

---
+++ rails/project_rails/app/controllers/comments_controller.rb
@@ -0,0 +1,9 @@
+class CommentsController < ApplicationController
+
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create(params[:comment].permit(:name,:body))
+    redirect_to '/post/show/#{params[:id]}'
+  end
+
+end
