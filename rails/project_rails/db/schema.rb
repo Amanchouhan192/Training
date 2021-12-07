833d53cc6db888113297329177938a72533736fa
amanchouhan192 <amanchouhan192@gmail.com>
2021-12-07 17:15:11 +0530

    added associations in post model with comment model and removed post css from the post.scss.

--- rails/project_rails/db/schema.rb
+++ rails/project_rails/db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_04_124727) do
+ActiveRecord::Schema.define(version: 2021_12_07_092127) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string "name"
+    t.text "body"
+    t.bigint "post_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -33,4 +42,5 @@ ActiveRecord::Schema.define(version: 2021_12_04_124727) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "comments", "posts"
 end
