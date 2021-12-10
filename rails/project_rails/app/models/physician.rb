9aac401df8c08551d7e529b86b8a00d1678fb5c9
amanchouhan192 <amanchouhan192@gmail.com>
2021-12-10 13:13:51 +0530

    added datatype for appointment_date.

--- rails/project_rails/app/models/physician.rb
+++ rails/project_rails/app/models/physician.rb
@@ -1,5 +1,7 @@
 class Physician < ApplicationRecord
     has_many :appointments
     has_many :patients, :through => :appointments
+    #validations
+    validates :name ,presence: true
 
 end
--- rails/project_rails/db/schema.rb
+++ rails/project_rails/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_10_060705) do
+ActiveRecord::Schema.define(version: 2021_12_10_070100) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,7 +24,7 @@ ActiveRecord::Schema.define(version: 2021_12_10_060705) do
   end
 
   create_table "appointments", force: :cascade do |t|
-    t.datetime "appointment_data"
+    t.datetime "appointment_date"
     t.bigint "patient_id"
     t.bigint "physician_id"
     t.datetime "created_at", precision: 6, null: false
