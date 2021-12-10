6ccca4348807a5c4b2edf99326795471f35aa754
amanchouhan192 <amanchouhan192@gmail.com>
2021-12-10 18:33:59 +0530

    changes in simple_cms and blog_project old changes were not committed.

--- rails/blog_project/db/schema.rb
+++ rails/blog_project/db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_03_135316) do
+ActiveRecord::Schema.define(version: 2021_12_08_044038) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "people", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
--- rails/simple_cms/app/controllers/pages_controller.rb
+++ rails/simple_cms/app/controllers/pages_controller.rb
@@ -1,18 +1,34 @@
 class PagesController < ApplicationController
   
   def index
+    @employee = Employee.order('name ASC')
   end
 
   def show
+    @employee = Subject.find(params[:id])
   end
 
   def new
+    @employee = Employee.new
   end
 
   def create
+     #Instantiate a new object using form parameter
+     @employee = Employee.new(params[:fname]) # mass assignment 
+     #save the object
+     if @employee.save 
+       #if save succeeds,redirect to the next index action
+       redirect_to(pages_path)
+     else
+       #if save fails ,redisplay the form so user can fix problem.
+       render('new')
+     end
   end
 
   def edit
+    @employee = Employee.find(params[:id])
+    #validates :form_checkbox, acceptance: {accept: true} , on: :create, 
+    #allow_nil: false
   end
 
   def update
