8e337059e1fbb9fb4539b4d74165fa2edd010f11
amanchouhan192 <amanchouhan192@gmail.com>
2021-12-10 10:47:01 +0530

    indent the user controller code and added address field for apply inclusion validation in params permit.

--- rails/project_rails/app/controllers/user_controller.rb
+++ rails/project_rails/app/controllers/user_controller.rb
@@ -2,27 +2,28 @@ class UserController < ApplicationController
 	def new
 		@user = User.new
 	end
-		def create
-		@user = User.new(user_params)
-		if @user.save
-			redirect_to '/user/new',success: "User Ragistered Successfully!"
-		else
+	
+	def create
+	  @user = User.new(user_params)
+	  	if @user.save
+		  	redirect_to '/user/new',success: "User Ragistered Successfully!"
+	    else
 			#redirect_to '/user/new' ,danger: "Invalid Email or Phone Number you entered! or Password mismatch!"
-			render 'new'
+		    render 'new'
 		end
-	end
+end
 
 	def login_check
 		@user = User.find_by(email: params[:user][:email]) 
 		if @user.present?
-			if @user.password == params[:user][:password]
+			if @user.password == params[:user][:password] 
 				session[:user_id] = @user.id  
 				redirect_to '/post/index'
 			else
 				redirect_to '/user/login' ,danger: "Invalid Email or Password!"
 			end
 		else
-			redirect_to '/user/login' ,danger: "Invalid Email or Password!"
+			render 'login'
 		end
 	end
 
@@ -40,6 +41,6 @@ class UserController < ApplicationController
 	private
 
 	def user_params
-		params.require(:user).permit(:name,:email,:password, :password_confirmation,:gender,:age,:phone)
+		params.require(:user).permit(:name,:email,:password, :password_confirmation,:gender,:age,:phone,:address)
 	end
 end
