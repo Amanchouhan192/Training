fa3d904e554b81a32d488b2b8cd2e966acbf289e
amanchouhan192 <amanchouhan192@gmail.com>
2021-12-16 14:52:29 +0530

    updated emp_id in schema while adding column.

--- rails/emp_management/db/schema.rb
+++ rails/emp_management/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_16_060255) do
+ActiveRecord::Schema.define(version: 2021_12_16_084213) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2021_12_16_060255) do
     t.string "department"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "emp_id"
   end
 
 end
