6f35b580beb7cb40da90e72c3ecfd6f5afa3bb93
amanchouhan192 <amanchouhan192@gmail.com>
2021-12-16 12:01:12 +0530

    add route in route.rb with get request for department and employee.

--- rails/emp_management/config/routes.rb
+++ rails/emp_management/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'department#index'
-  get 'home/index',to: 'home#index'
+  root 'departments#index'
+  get 'departments/index',to: 'departments#index'
+
+  get 'employee/new',to: 'employee#new'
 end
