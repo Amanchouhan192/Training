d701b68c56f97a0ce71e21b09a3d3feac3410774
amanchouhan192 <amanchouhan192@gmail.com>
2021-12-16 16:01:12 +0530

    update route for department controller and action.

--- rails/emp_management/config/routes.rb
+++ rails/emp_management/config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'departments#index'
-  get 'departments/index',to: 'departments#index'
+  root 'employee#index'
+  get 'employee/index',to: 'employee#index'
 
   get 'employee/new',to: 'employee#new'
 
+  get '/departments/index',to: 'departments#index'
+  
   post 'employee/create', to: 'employee#create'
 end
