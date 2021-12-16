318db156128392a3b8fc64be0e8cec45c52a3c07
amanchouhan192 <amanchouhan192@gmail.com>
2021-12-16 11:57:47 +0530

    create department action index schema updated while employee model migration executed.

--- rails/emp_management/app/controllers/departments_controller.rb
+++ rails/emp_management/app/controllers/departments_controller.rb
@@ -1,2 +1,4 @@
 class DepartmentsController < ApplicationController
+    def index
+    end
 end
--- rails/emp_management/db/schema.rb
+++ rails/emp_management/db/schema.rb
@@ -10,13 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_16_055324) do
+ActiveRecord::Schema.define(version: 2021_12_16_060255) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "departments", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "employees", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "gender"
+    t.integer "salary"
+    t.string "department"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
