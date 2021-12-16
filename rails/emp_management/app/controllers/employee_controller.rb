7aeeafa2a4f227c1694ca899feed2c9bc08cf4ef
amanchouhan192 <amanchouhan192@gmail.com>
2021-12-16 12:59:02 +0530

    create form functionality on create action and add employee params permit defination.

--- rails/emp_management/app/controllers/employee_controller.rb
+++ rails/emp_management/app/controllers/employee_controller.rb
@@ -4,5 +4,17 @@ class EmployeeController < ApplicationController
   end
   
   def create
+    @employee = Employee.new(employee_params)
+	  if @employee.save
+        redirect_to '/employee/new'
+	  else
+	    render 'new'
+	  end
+    end
+
+  private
+
+  def employee_params
+    params.require(:employee).permit(:name,:email,:gender,:salary,:department,:email)
   end
 end
