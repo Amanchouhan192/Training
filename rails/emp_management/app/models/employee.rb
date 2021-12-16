98ae7f9d4d58d5f210424e3ae99812832be6f5fe
amanchouhan192 <amanchouhan192@gmail.com>
2021-12-16 16:20:25 +0530

    removed emp_id from validation.

--- rails/emp_management/app/models/employee.rb
+++ rails/emp_management/app/models/employee.rb
@@ -7,7 +7,7 @@ class Employee < ApplicationRecord
   validates :name, format: { with: /\A[a-zA-Z]+\z/,message: "Should Be Valid Name" ,length: {minimum: 6}}
 
   validates :email, uniqueness: true
-  validates :emp_id, uniqueness: {:message => " Id is already been taken!"}
+  #validates :emp_id, uniqueness: {:message => " Id is already been taken!"}
 
   validates :email, format: { with: /^[\w.+\-]+@gmail.com$/,:multiline => true ,message: "must be a valid email address", on: :create }
 
