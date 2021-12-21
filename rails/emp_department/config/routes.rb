e67184dda0b386a828e350a4ecf39a4a21352f8e
amanchouhan192 <amanchouhan192@gmail.com>
2021-12-21 13:21:24 +0530

    add routes.

--- rails/emp_department/config/routes.rb
+++ rails/emp_department/config/routes.rb
@@ -1,4 +1,24 @@
 Rails.application.routes.draw do
+
+  root 'employee#index'
+  get 'employee/index',to: 'employee#index'
+
+  get 'employee/new',to: 'employee#new'
+
+  get '/departments/index',to: 'departments#index'
+
+  get '/departments/show',to: 'departments#show'
+  
+  post 'employee/create', to: 'employee#create'
+=begin
+  get 'employee/index'
+  get 'employee/new'
+  get 'employee/show'
+  get 'employee/create'
+  get 'employee/edit'
+  get 'employee/destroy'
+  get 'employee/delete'
   resources :departments
+=end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
