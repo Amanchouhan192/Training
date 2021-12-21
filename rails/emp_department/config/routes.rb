61b4ea548f0ed847f0db56e3c118515c4c31812c
amanchouhan192 <amanchouhan192@gmail.com>
2021-12-21 16:54:45 +0530

    removed unwanted routes.

--- rails/emp_department/config/routes.rb
+++ rails/emp_department/config/routes.rb
@@ -1,24 +1,8 @@
 Rails.application.routes.draw do
-
-  root 'employee#index'
-  get 'employee/index',to: 'employee#index'
-
-  get 'employee/new',to: 'employee#new'
-
-  get '/departments/index',to: 'departments#index'
-
-  get '/departments/show',to: 'departments#show'
-  
-  post 'employee/create', to: 'employee#create'
-=begin
-  get 'employee/index'
-  get 'employee/new'
-  get 'employee/show'
-  get 'employee/create'
-  get 'employee/edit'
-  get 'employee/destroy'
-  get 'employee/delete'
   resources :departments
-=end
+  root 'employee#index'
+  get 'employee/index', to: 'employee#index'
+  get 'employee/new',to: 'employee#new'
+  post 'employee/create', to: 'employee#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
