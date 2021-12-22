613c701733883db135fa8bc224ab3606e1af18b7
amanchouhan192 <amanchouhan192@gmail.com>
2021-12-22 11:51:19 +0530

    update schema.rb while rename the model.

--- rails/emp_department/db/schema.rb
+++ rails/emp_department/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_21_073137) do
+ActiveRecord::Schema.define(version: 2021_12_22_061245) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2021_12_21_073137) do
   create_table "departments", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "dept"
+    t.string "name"
   end
 
   create_table "employees", force: :cascade do |t|
