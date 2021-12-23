930764f10274ca2c01c7869e31d206a947a15553
amanchouhan192 <amanchouhan192@gmail.com>
2021-12-23 11:55:22 +0530

    update schema.rb while updating the model.

--- rails/survey_application/db/migrate/20211223055045_create_surveys.rb
+++ rails/survey_application/db/migrate/20211223055045_create_surveys.rb
@@ -2,7 +2,7 @@ class CreateSurveys < ActiveRecord::Migration[6.1]
   def change
     create_table :surveys do |t|
       t.string :name
-
+      
       t.timestamps
     end
   end
--- rails/survey_application/db/schema.rb
+++ rails/survey_application/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_23_055235) do
+ActiveRecord::Schema.define(version: 2021_12_23_061729) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,12 +19,16 @@ ActiveRecord::Schema.define(version: 2021_12_23_055235) do
     t.string "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "question_id"
+    t.index ["question_id"], name: "index_answers_on_question_id"
   end
 
   create_table "questions", force: :cascade do |t|
     t.string "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "survey_id"
+    t.index ["survey_id"], name: "index_questions_on_survey_id"
   end
 
   create_table "surveys", force: :cascade do |t|
@@ -33,4 +37,6 @@ ActiveRecord::Schema.define(version: 2021_12_23_055235) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "answers", "questions"
+  add_foreign_key "questions", "surveys"
 end
