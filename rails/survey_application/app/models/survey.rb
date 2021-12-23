213ad8940dc7f482cff4a9d614d977d927aa91f1
amanchouhan192 <amanchouhan192@gmail.com>
2021-12-23 19:27:24 +0530

    removed unwanted code from survey controller.

--- rails/survey_application/app/models/survey.rb
+++ rails/survey_application/app/models/survey.rb
@@ -1,6 +1,4 @@
 class Survey < ApplicationRecord
-  attr_accessor :name, :questions_attributes
-  #attr_accessible :name, :questions_attributes
   has_many :questions
   accepts_nested_attributes_for :questions
 end
