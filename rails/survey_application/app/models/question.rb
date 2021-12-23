b9bb4be15180fd75fd8fd3c2c719923b44bf6b8f
amanchouhan192 <amanchouhan192@gmail.com>
2021-12-23 19:30:03 +0530

    removed unwanted code from question controller.

--- rails/survey_application/app/models/question.rb
+++ rails/survey_application/app/models/question.rb
@@ -1,6 +1,5 @@
 class Question < ApplicationRecord
     belongs_to :survey
     has_many :answers
-    attr_accessor :content, :survey_id, :answers_attributes
     accepts_nested_attributes_for :answers, allow_destroy: true
 end
