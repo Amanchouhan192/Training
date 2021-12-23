1a7f23be749a57887ada1d10066046e1722ef826
amanchouhan192 <amanchouhan192@gmail.com>
2021-12-23 17:26:46 +0530

    add attr_accessor and nested accepts_nested_attributes_for questions in survey model.

--- rails/survey_application/app/models/survey.rb
+++ rails/survey_application/app/models/survey.rb
@@ -1,3 +1,6 @@
 class Survey < ApplicationRecord
-    has_many :questions
+  attr_accessor :name, :questions_attributes
+  #attr_accessible :name, :questions_attributes
+  has_many :questions
+  accepts_nested_attributes_for :questions
 end
