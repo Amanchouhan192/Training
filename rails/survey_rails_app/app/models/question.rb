84d7b048e9ba9d142954ca9ebd87e17118d820cd
amanchouhan192 <amanchouhan192@gmail.com>
2021-12-26 16:31:10 +0530

    add associations in questions.

--- rails/survey_rails_app/app/models/question.rb
+++ rails/survey_rails_app/app/models/question.rb
@@ -1,5 +1,5 @@
 class Question < ApplicationRecord
 	belongs_to :survey
-    has_many :answers
-    accepts_nested_attributes_for :answers, allow_destroy: true
+    has_many :answers, :dependent => :destroy
+    accepts_nested_attributes_for :answers
 end
