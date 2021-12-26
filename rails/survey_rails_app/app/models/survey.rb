28a8929523b04f06dc87085160ef8ad9fe4a3535
amanchouhan192 <amanchouhan192@gmail.com>
2021-12-27 00:28:45 +0530

    add validation in survey.

--- rails/survey_rails_app/app/models/survey.rb
+++ rails/survey_rails_app/app/models/survey.rb
@@ -1,4 +1,7 @@
 class Survey < ApplicationRecord
 	has_many :questions, :dependent => :destroy
   accepts_nested_attributes_for :questions 
+  #validations
+  validates :name, presence: true 
+  validates :name, uniqueness: true
 end
