9e77eac64af6b45decead3db6f74a1a42067006f
amanchouhan192 <amanchouhan192@gmail.com>
2021-12-26 16:27:48 +0530

    add associations.

--- rails/survey_rails_app/app/models/survey.rb
+++ rails/survey_rails_app/app/models/survey.rb
@@ -1,4 +1,4 @@
 class Survey < ApplicationRecord
-	has_many :questions
-  accepts_nested_attributes_for :questions
+	has_many :questions, :dependent => :destroy
+  accepts_nested_attributes_for :questions 
 end
